468b32f0e7aec73a13e0216497a61282d635d087
Tarho <115691708+Tarho@users.noreply.github.com>
2023-11-29 23:13:32 +0700

    Delete src/routes/product.js

--- src/routes/product.js
+++
@@ -1,8 +0,0 @@
-// src/routes/product.js
-const express = require("express");
-const router = express.Router();
-const productControllers = require("../controller/product");
-
-router.get("/product", productControllers.getAll);
-router.get("/product/:id", productControllers.getOne);
-module.exports = router;
