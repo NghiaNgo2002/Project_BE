e9df4950a4e09eb0744ed58fb2d250c68ee587a8
Tarho <20521935@gm.uit.edu.vn>
2023-12-03 00:34:43 +0700

    no message

--- src/app.js
+++ src/app.js
@@ -1,62 +1,58 @@
-const express = require('express');
-const LogInRoutes = require('./routes/LogIn');
+const express = require("express");
+const LogInRoutes = require("./routes/LogIn");
 const app = express();
-const RegisterRoutes = require('./routes/Register');
-const cors = require('cors');
-const swaggerSpec = require('./swagger'); // Import your Swagger specification
-const swaggerUi = require('swagger-ui-express'); // Import swagger-ui-express
-const profileRoutes = require('./routes/profile');
-const { verifyToken } = require('./middleware/authMiddleware');
-const passwordRoutes = require('./routes/Password');
-const EditProfileRoutes = require('./routes/profile')
-const ListAllprofileRoutes = require('./routes/profile-admin')
-const productRoutes = require('./routes/product');
-const productdetailRoutes = require('./routes/productdetail');
+const RegisterRoutes = require("./routes/Register");
+const cors = require("cors");
+const swaggerSpec = require("./swagger"); // Import your Swagger specification
+const swaggerUi = require("swagger-ui-express"); // Import swagger-ui-express
+const profileRoutes = require("./routes/profile");
+const { verifyToken } = require("./middleware/authMiddleware");
+const passwordRoutes = require("./routes/Password");
+const EditProfileRoutes = require("./routes/profile");
+const ListAllprofileRoutes = require("./routes/profile-admin");
+const productRoutes = require("./routes/product");
+const productdetailRoutes = require("./routes/productdetail");
 
-require('dotenv').config();
+require("dotenv").config();
 
 // Use built-in middleware for json
 app.use(express.json());
 
-
 // Serve Swagger documentation using Swagger UI
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
- // CORS configuration
-// Replace 'https://your-frontend.vercel.app' with your actual frontend application URL
+// CORS configuration
 const corsOptions = {
-  origin: ['http://localhost:3000'],
-  optionsSuccessStatus: 200
+  origin: ["http://localhost:3000"],
+  optionsSuccessStatus: 200,
 };
+
+app.use(cors());
 
 // Enable CORS with the above options
 app.use(cors(corsOptions));
-
 
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 
 // Configure routes without passing the db object
-app.use('/api',LogInRoutes);
-app.use('/api',RegisterRoutes);
-app.use('/api/profile', verifyToken, profileRoutes); // Protect profile routes
-app.use('/api',verifyToken,passwordRoutes);
-app.use('/api/profile',verifyToken,EditProfileRoutes);
-app.use('/api/profile-admin', verifyToken, ListAllprofileRoutes);
-app.use('/api',verifyToken,productRoutes);
-app.use('/api',verifyToken,productdetailRoutes);
+app.use("/api", LogInRoutes);
+app.use("/api", RegisterRoutes);
+app.use("/api/profile", verifyToken, profileRoutes);
+app.use("/api", verifyToken, passwordRoutes);
+app.use("/api/profile", verifyToken, EditProfileRoutes);
+app.use("/api/profile-admin", verifyToken, ListAllprofileRoutes);
+app.use("/api", productRoutes);
+app.use("/api", verifyToken, productdetailRoutes);
 
-
-
-
-// Error handling middleware (example)
-app.use((err,req,res,next) =>{
-    console.error(err.stack);
-    res.status(500).send ('Some thing broke!');
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
 });
 
 // Add the app.listen method to start the server
-const port = process.env.PORT || 3001; // Specify the port you want to listen on
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,31 +1,29 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
-
+// Bỏ xác thực token nếu endpoint không yêu cầu
 const verifyToken = (req, res, next) => {
-  // Get the token from the Authorization header
   const authHeader = req.headers.authorization;
-  console.log('Authorization Header:', authHeader);
+  console.log("Authorization Header:", authHeader);
 
-  if (authHeader) {
-    const token = authHeader.split(' ')[1]; // Authorization: 'Bearer TOKEN'
-
-    // Verify the token using the JWT secret from your .env file
-    jwt.verify(token, process.env.JWT_SECRET, (err, accounts) => {
-      if (err) {
-        console.error('Error during token verification:', err);
-        if (err.name === 'TokenExpiredError') {
-          return res.status(401).json({ message: 'Token expired' });
-        } else {
-          return res.status(403).json({ message: 'Forbidden: Invalid token' });
-        }
-      }
-      req.accounts = accounts;
-      next();
-    });
-  } else {
-    // If no token is provided, send a forbidden status
-    res.sendStatus(401);
+  // Kiểm tra nếu không có token
+  if (!authHeader) {
+    return next();
   }
+
+  const token = authHeader.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+
+  jwt.verify(token, process.env.JWT_SECRET, (err, accounts) => {
+    if (err) {
+      console.error("Error during token verification:", err);
+      if (err.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "Token expired" });
+      } else {
+        return res.status(403).json({ message: "Forbidden: Invalid token" });
+      }
+    }
+    req.accounts = accounts;
+    next();
+  });
 };
 
 module.exports = { verifyToken };
