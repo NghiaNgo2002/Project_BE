fd492657c7eec79b27e39a0133d2710ec7dfa613
NghiaNgo2002 <nghiango229@gmail.com>
2023-12-30 18:00:04 +0700

    123

--- src/swagger.js
+++ src/swagger.js
@@ -11,17 +11,17 @@ const options = {
     },
   },
   // List of files to be processed by swagger-jsdoc
-  apis: ['./routes/LogIn.js',
-  './routes/Register.js',
-  './routes/adminproduct.js',
-  './routes/cart.js',
-  './routes/order.js',
-  './routes/orderdetail.js',
-  './routes/Password.js',
-  './routes/product.js',
-  './routes/productdetail.js',
-  './routes/profile-admin.js',
-  './routes/profile.js']
+  apis: ['/LogIn.js',
+  '/Register.js',
+  '/adminproduct.js',
+  '/cart.js',
+  '/order.js',
+  '/orderdetail.js',
+  '/Password.js',
+  '/product.js',
+  '/productdetail.js',
+  '/profile-admin.js',
+  '/profile.js']
 };
 
 const swaggerSpec = swaggerJsdoc(options);
